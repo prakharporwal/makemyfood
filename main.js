c61081bef6d80f8882c8be406c2adcc4a424750e
prakharporwal <rsanshul99@gmail.com>
2022-07-07 10:28:10 +0530

    form validation no empty entry added

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-function submitWaitListForm() {
-  var name = document.getElementById("full-name");
-  var email = document.getElementById("email");
+// function submitWaitListForm() {
+//   var name = document.getElementById("full-name");
+//   var email = document.getElementById("email");
 
-  if (!validateForm(name, email)) {
+//   if (!validateForm(name, email)) {
+//   }
+
+//   return;
+// }
+
+// USING PAGECLIP TO SUBMIT FORM
+
+function validateForm() {
+  let fullName = document.getElementById("full-name");
+  let email = document.getElementById("email");
+
+  if (fullName === "") {
+    return false;
   }
-
-  trigger;
-
-  return;
+  return validateEmail(email);
 }
 
-function validateForm() {}
+function validateEmail(mail) {
+  if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)
+  ) {
+    return true;
+  }
+  alert("You have entered an invalid email address!");
+  return false;
+}
 
 $(".join-button-scroll").click(function () {
   $("html,body").animate(
